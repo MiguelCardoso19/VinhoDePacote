60b8377469230c5de61974ac80175aa2f1e14606
Miguel Cardoso <145140578+MiguelCardoso19@users.noreply.github.com>
2024-02-16 09:45:47 +0000

    Add files via upload
    Added Coin Class, Implemented Score System, Added Heart Class Updated die() method in Pacman.

---
+++ Coin.java
@@ -0,0 +1,26 @@
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public class Coin {
+    private int x;
+    private int y;
+    private Picture coinPicture;
+
+    public Coin(int x, int y, String picturePath) {
+        this.x = x;
+        this.y = y;
+        this.coinPicture = new Picture(x, y, picturePath);
+        this.coinPicture.draw();
+    }
+
+    public void delete() {
+        coinPicture.delete();
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+        public static void main(String[] args) {
+
+            Grid grid = new Grid(10, 10);
+        }
+    }
+
