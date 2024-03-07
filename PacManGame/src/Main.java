aad9b312ad719ea2cae2875d25dd62ea992acfc7
Miguel Cardoso <145140578+MiguelCardoso19@users.noreply.github.com>
2024-03-07 13:27:36 +0000

    Delete PacManGame directory

--- PacManGame/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-        public static void main(String[] args) throws InterruptedException {
-
-            Grid grid = new Grid(10, 10);
-            grid.init();
-        }
-    }
-
